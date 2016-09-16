12d4328065518dd044786c8a4fe437288b1015c2
aamir <aamirmaj@udel.edu>
2016-09-16 12:23:00 -0400

    Angular Stuff

--- hello.js
+++ hello.js
@@ -1,27 +1,36 @@
-$(document).ready(function(){
-  var AppRouter = Backbone.Router.extend({
-    routes : {
-      "" : "loadHome",
-      "home" : "loadHome",
-      "page1" : "loadPage1",
-      "*actions" : "defaultRoute"
+var app = angular
+  .module("singlePageApp", ['ngRoute']);
+app .config([
+  "$routeProvider",
+  function($routeProvider){
+    $routeProvider
+    	.when('/',{
+    	  templateUrl: "home.html",
+        controller : "homeController"
+      	})
+      	.when('/page1',{
+      	 templateUrl: "page1.html",
+      	 controller : "page1Controller"
+      	})
+      	.otherwise({
+      	 redirectTo : "/"
+      	});
+}]);
+
+app.controller(
+  "homeController",
+  ["$scope",
+  	function($scope){
+  		$scope.message = "home page loaded";
     }
-  });
+  ]
+);
 
-  var app_router = new AppRouter();
-
-  app_router.on("route:loadHome", function(){
-    $("body").html("<h2>Home Page</h2><a href=\"#page1\">Link to page1</a>");
-  });
-
-  app_router.on("route:loadPage1", function(){
-    $("body").html("<h2>Page One</h2><a href=\"#home\">Link to home page</a>");
-  });
-
-  app_router.on("route:defaultRoute", function(){
-    $("body").html("<h2>Error Page</h2>Not a valid route... <a href=\"#home\">Link to home page</a>");
-  });
-
-  Backbone.history.start();
-
-});
+app.controller(
+  "page1Controller",
+  ["$scope",
+  	function($scope){
+  		$scope.message = "page one loaded";
+    }
+  ]
+);
